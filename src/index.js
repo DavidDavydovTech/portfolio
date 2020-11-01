1df099a8869acfe121149f2b3439b70c89fbb1ba
DavidDavydovTech <davydovjd611@gmail.com>
2020-11-01 10:52:47 -0500

    Add CSS and boilerplate from mithril website

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import m from 'mithril';
 
-m.render(document.body, 'hello world');
+var root = document.body
+var count = 0
+
+var Hello = {
+    view: function() {
+        return m("main", [
+            m("h1", {
+                class: "title"
+            }, "My first app"),
+            m("button", {
+                onclick: function() {count++}
+            }, count + " clicks"), 
+            m("div", {
+                class: "box"
+            }, "My first app"),
+        ])
+    }
+}
+
+var Splash = {
+    view: function() {
+        return m("a", {
+            href: "#!/hello"
+        }, "Enter!")
+    }
+}
+
+m.route(root, "/splash", {
+    "/splash": Splash,
+    "/hello": Hello,
+})
+
