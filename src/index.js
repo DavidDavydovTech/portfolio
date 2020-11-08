acc262edb6c4f96bf4bb8261a596762a5c307ec1
David J. Davydov <davydovjd611@gmail.com>
2020-11-07 23:01:01 -0500

    Final commit for the night

--- src/index.js
+++ src/index.js
@@ -1,58 +1,46 @@
 import m from 'mithril';
-
 // var ProjectIcon = {
 //     view: (img) => (
 //         <div class='box' style={`background-image: url('${img}')`}/>
 //     )
 // }
+var root = document.body
+var count = 0 // added a variable
 
-m.render( document.body, 
-  m(
-    'div',
-    {
-      id: 'viewport',
-    },
-    [
-      m( 'nav',
-        {},
-        [
-          // m(
-          //   'button',
-          //   {
-          //     class: 'navbar-toggler',
-          //     type: 'button',
-          //     'aria-label': 'Toggle navigation'
-          //   }
-          // ),
-          m(
-            'ul',
-            {},
-            [
-              'nav'
-            ]
-          ),
-        ]
-      ),
-      m(
-        'div',
-        {
-          id: 'subport'
-        },
-        null
-      )
-    ]
+var App = {
+  view: () => (
+    <>
+      <nav>
+        nav
+      </nav>
+      <div id='content'>
+        An unknown error occured
+      </div>
+    </>
   )
-);
+}
+m.mount(root, App)
 
+const Home = {
+  view: () => (
+    <div>
+      Hello!!!
+      <button>
+        Click me!
+      </button>
+    </div>
+  )
+}
 
-m.route(document.getElementById('subport'), 
+// Make routes use no prefix
+m.route.prefix = '';
+// Get the content element
+let contentDiv = document.getElementById('content');
+// Set up routes
+m.route(contentDiv, 
     '/home', 
     {
-        '/home': m(
-            'div',
-            {},
-            'HELLO'
-        ),
+        '/home': Home,
     }
 )
 
