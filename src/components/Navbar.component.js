6c397d283156167e092526c8e33ef1a981d86afe
David J. Davydov <davydovjd611@gmail.com>
2020-11-07 22:21:51 -0500

    Move navbar to index.js

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -3,24 +3,19 @@ import m from 'mithril';
 const Navbar = {
   view: function (vnode) {
     return m( 'nav',
-      {
-          id: 'tmSidebar',
-          class: 'tm-bg-black-transparent tm-sidebar',
-      },
+      {},
       [
+          // m(
+          //   'button',
+          //   {
+          //     class: 'navbar-toggler',
+          //     type: 'button',
+          //     'aria-label': 'Toggle navigation'
+          //   }
+          // ),
           m(
-            'button',
-            {
-              class: 'navbar-toggler',
-              type: 'button',
-              'aria-label': 'Toggle navigation'
-            }
-          ),
-          m(
-            'div',
-            {
-              class: 'tm-sidebar-sticky',
-            },
+            'ul',
+            {},
             [
               
             ]
--- src/index.js
+++ src/index.js
@@ -1,37 +1,58 @@
 import m from 'mithril';
-import Navbar from './components/Navbar.component';
-var root = document.body
 
 // var ProjectIcon = {
 //     view: (img) => (
-//         <div class="box" style={`background-image: url("${img}")`}/>
+//         <div class='box' style={`background-image: url('${img}')`}/>
 //     )
 // }
 
-var Hello = {
-    view:  function () {
-        return m(
-        
-        );
-    }
-}
-
-var Splash = {
-    view: function() {
-        return m(m.route.Link, {
-            href: "/hello"
-        }, "Enter!")
-    }
-}
-
-m(
-    root,
-    {},
+m.render( document.body, 
+  m(
+    'div',
+    {
+      id: 'viewport',
+    },
     [
-        Navbar,
-        m.route(root, "/home", {
-            "/home": Navbar,
-        })
+      m( 'nav',
+        {},
+        [
+          // m(
+          //   'button',
+          //   {
+          //     class: 'navbar-toggler',
+          //     type: 'button',
+          //     'aria-label': 'Toggle navigation'
+          //   }
+          // ),
+          m(
+            'ul',
+            {},
+            [
+              'nav'
+            ]
+          ),
+        ]
+      ),
+      m(
+        'div',
+        {
+          id: 'subport'
+        },
+        null
+      )
     ]
+  )
+);
+
+
+m.route(document.getElementById('subport'), 
+    '/home', 
+    {
+        '/home': m(
+            'div',
+            {},
+            'HELLO'
+        ),
+    }
 )
 
