298858a0b7bb6c8911276099e27f6b11d3f9da2b
David J. Davydov <davydovjd611@gmail.com>
2020-11-27 06:36:27 -0500

    Make the menu appear when clicking the toggle btn

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ class Navbar extends React.Component {
     super(props);
 
     this.state={
-      isOpen: true,
+      isOpen: false,
     };
 
     // Bind methods
@@ -31,14 +31,19 @@ class Navbar extends React.Component {
   }
 
   render() {
+    const { isOpen } = this.state;
     return (
       <>
         <nav className="fixed z-30 flex justify-between align-bottom w-screen font-mono p-4 px-8 text-3xl text-gray-900 bg-gray-200 dark:text-gray-100 dark:bg-gray-800 shadow-2xl">
           <Logo />
           {this.renderMenuButton()}
         </nav>
-        <div>
-          
+        <div className="z-20 fixed transition-all duration-500 ease-in-out w-screen h-screen bg-gray-200 dark:bg-gray-800"
+          style={{
+            left: isOpen ? '0%' : '100%',
+          }}
+        >
+          bye
         </div>
       </>
     );
