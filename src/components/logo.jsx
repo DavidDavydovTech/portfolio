56f75014f02d3103263d762b3f085b7a1cb03f10
David J. Davydov <davydovjd611@gmail.com>
2020-11-27 04:45:51 -0500

    Move logo to its own functional component

---
+++ src/components/logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo = () => (
+    <a>
+        David Davydov
+        <a className="text-base text-gray-200 font-600">
+            <a className="text-green-400 font-black"> .</a>
+            tech
+            <a className="text-gray-700">nology</a>
+        </a>
+    </a>
+);
+
+export default Logo;
