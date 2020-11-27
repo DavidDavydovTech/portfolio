93654f3dcac9f7e6c6be5436f9789a1e9014bf9e
David J. Davydov <davydovjd611@gmail.com>
2020-11-27 05:28:22 -0500

    Add a renderMenuButton method

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // Components
 import Logo from './logo';
 // Icons
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -11,13 +11,24 @@ class Navbar extends React.Component {
     this.state={
       isOpen: false,
     };
+
+    // Bind methods
+    this.renderMenuButton = this.renderMenuButton.bind(this);
+  }
+
+  renderMenuButton() {
+    const { isOpen } = this.state;
+    if (isOpen) {
+      return <FaTimes className="cursor-pointer pt-1.5"/>;
+    }
+    return <FaBars className="cursor-pointer pt-1.5"/>;
   }
 
   render() {
     return (
-      <nav className="fixed z-30 w-screen font-mono p-4 pl-8 text-3xl text-gray-900 bg-gray-200 dark:text-gray-100 dark:bg-gray-800 shadow-2xl">
-        <h1><Logo /></h1>
-        <FaBars />
+      <nav className="fixed z-30 flex justify-between align-bottom w-screen font-mono p-4 px-8 text-3xl text-gray-900 bg-gray-200 dark:text-gray-100 dark:bg-gray-800 shadow-2xl">
+        <Logo />
+        {this.renderMenuButton}
       </nav>
     );
   }
