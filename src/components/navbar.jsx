7fc578ed18061a96d07011c7d23c3f42010ee14a
David J. Davydov <davydovjd611@gmail.com>
2020-11-27 06:25:44 -0500

    Add a menu toggler button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,27 +9,38 @@ class Navbar extends React.Component {
     super(props);
 
     this.state={
-      isOpen: false,
+      isOpen: true,
     };
 
     // Bind methods
+    this.toggleMenu = this.toggleMenu.bind(this);
     this.renderMenuButton = this.renderMenuButton.bind(this);
+  }
+
+  toggleMenu(e) {
+    const { isOpen } = this.state;
+    this.setState({isOpen: !isOpen});
   }
 
   renderMenuButton() {
     const { isOpen } = this.state;
     if (isOpen) {
-      return <FaTimes className="cursor-pointer pt-1.5"/>;
+      return <FaTimes onClick={this.toggleMenu} className="cursor-pointer pt-1.5"/>;
     }
-    return <FaBars className="cursor-pointer pt-1.5"/>;
+    return <FaBars onClick={this.toggleMenu} className="cursor-pointer pt-1.5"/>;
   }
 
   render() {
     return (
-      <nav className="fixed z-30 flex justify-between align-bottom w-screen font-mono p-4 px-8 text-3xl text-gray-900 bg-gray-200 dark:text-gray-100 dark:bg-gray-800 shadow-2xl">
-        <Logo />
-        {this.renderMenuButton}
-      </nav>
+      <>
+        <nav className="fixed z-30 flex justify-between align-bottom w-screen font-mono p-4 px-8 text-3xl text-gray-900 bg-gray-200 dark:text-gray-100 dark:bg-gray-800 shadow-2xl">
+          <Logo />
+          {this.renderMenuButton()}
+        </nav>
+        <div>
+          
+        </div>
+      </>
     );
   }
 }
