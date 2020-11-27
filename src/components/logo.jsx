6ad43a75352b8dad4ae471f6dc43ae5bb7440387
David J. Davydov <davydovjd611@gmail.com>
2020-11-27 17:33:13 -0500

    Change the logo in lightmode and make the '.' bigger

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -4,9 +4,9 @@ const Logo = () => (
     <a>
         David Davydov
         <a className="text-sm md:text-base dark:text-gray-200">
-            <a className="text-green-400 font-black"> .</a>
+            <b className="text-green-400 font-black text-3xl">.</b>
             tech
-            <a className="text-gray-300 dark:text-gray-600 animate-pulse">nology</a>
+            <a className="text-gray-400 dark:text-gray-600 animate-pulse">nology</a>
         </a>
     </a>
 );
