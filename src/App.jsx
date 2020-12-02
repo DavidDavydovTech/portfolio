91e7aeb0ebd10da2cc7873dca50c05e1c3f9fc68
David J. Davydov <davydovjd611@gmail.com>
2020-12-02 14:23:09 -0500

    Add a default title and description

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import { Helmet } from 'react-helmet';
 // Components
 import Navbar from './components/navbar';
 import NavSpacer from './components/navspacer';
@@ -9,6 +10,13 @@ import HomePage from './components/home.page';
 
 const App = () => (
   <>
+    <Helmet>
+        <title>David J. Davydov</title>
+        <meta 
+          name="description" 
+          content="The website of software engineer David J. Davydov. Soon to feature a e-resume, dev blogs, and hosted projects." 
+        />
+    </Helmet>
     <Navbar />
     <div className="z-0 fixed w-screen h-screen bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100 overflow-y-scroll">
       <NavSpacer />
