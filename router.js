a72d9156b5f20c231c753c44afd868ba02c52ad6
YBO8001 <daviddavydov@northwesternmutual.com>
2021-03-31 21:34:02 -0400

    Make some fixes to the file

--- router.js
+++ router.js
@@ -1,18 +1,10 @@
 // -- Env Vars --
-// This initalizes our env file.
+// This initializes our env file.
 require('dotenv').config();
-const {
-  API_SUBDOMAIN,
-  API_PORT,
-  // CLIENT_SUBDOMAIN,
-  CLIENT_PORT,
-} = process.env;
-
-console.log(API_SUBDOMAIN, API_PORT)
 
 // -- Package Imports --
 const { readdirSync } = require('@jsdevtools/readdir-enhanced');
-const { join } = require('path');
+// const { join } = require('path');
 const { Router } = express = require('express');
 const expressSubdomain = require('express-subdomain');
 const proxy = require('express-http-proxy');
@@ -25,22 +17,23 @@ const router = Router();
 
 
 // Dynamically add methods/routes to API
-const siteConfigs = readdirSync('../sites', {deep: 2, filter: /.*\.site/, });
+const siteConfigs = readdirSync('./sites', {deep: 2, filter: /.*\.site/, });
 const usedSubdomains = [];
 console.log('\nINITALIZING ROUTES:\n')
 for (let siteConfig of siteConfigs) {
   let siteApp;
+  console.log(siteConfig)
   const {
     type,
     location,
     subdomains
-  } = site = require(siteConfig);
+  } = site = require(`./sites/${siteConfig}`);
   // Initialize the 'App' for this site.
   if (type.toLowerCase() === 'file') {
-    siteApp = require(location);
+    siteApp = require(`./sites/${siteConfig}/${location}`);
   } else if (type.toLowerCase() === 'url') {
     siteApp = proxy(location);
-  } else throw new Error(`Invalid .site type ${type}<${typeof type}>`)
+  } else throw new Error(`Invalid .site type "${type}"<${typeof type}>.`)
   // Now assign the 'App' to every subdomain given.
   subdomains.forEach( subdomain => {
     let useSubdomain = subdomain !== '__DEFAULT__';
